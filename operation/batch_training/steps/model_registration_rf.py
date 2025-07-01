cbbe2eed7f364a8353c2f4aa0f3a612bbb85ce3e
Luis Armando Lazarte <la.lazartep@alum.up.edu.pe>
2025-07-01 05:53:23 -0500

    ci cd batch inference

--- operation/batch_training/steps/model_registration_rf.py
+++ operation/batch_training/steps/model_registration_rf.py
@@ -3,7 +3,7 @@ from sagemaker.workflow.function_step import step
 
 instance_type = "ml.m5.2xlarge"
 default_path = DEFAULT_PATH
-image_uri = "762233743642.dkr.ecr.us-east-2.amazonaws.com/vecolazarte/batch-training@sha256:0db3705aa34e0b5fa57d94f8822541e94df35ecc3606a46c41dfe9e14e12d101"
+image_uri = "762233743642.dkr.ecr.us-east-2.amazonaws.com/vecolazarte/batch-training@sha256:24382aa768fc52e7f4fd2cad252d276e4b456adee181bc7f65588be5db6ea947"
 
 @step(
     name="RegisterRandomForestModel", 
